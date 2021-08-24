fb2ab2e0116948df4503591b6f2d147a15be742b
Pr0moK <pr0mok1234@gmail.com>
2021-08-24 17:38:14 +0200

    Create PaperScissorsRockGame.py

---
+++ PaperScissorsRockGame.py
@@ -0,0 +1,30 @@
+import random
+
+list = ["paper", "rock", "scissors"]
+IA_choice = random.choice(list)
+while True:
+
+    User_Choice = input("paper, rock or scissors? ").lower()
+
+    print(f"Okay so User choose {User_Choice} and the IA choose {IA_choice} ")
+
+    if User_Choice == IA_choice:
+        print("It's a draw")
+    elif User_Choice == "rock":
+        if IA_choice == "scissors":
+            print("User won!")
+        else:
+            print("IA Won")
+    elif User_Choice == "scissors":
+        if IA_choice == "Rock":
+            print("User won!")
+        else:
+            print("IA Won")
+    elif User_Choice == "paper":
+        if IA_choice == "rock":
+            print("User Won")
+        else:
+            print("IA won")
+    play_again = input("Wanna play again? (Y)es/(N)o ").upper()
+    if play_again != "Y":
+        break
