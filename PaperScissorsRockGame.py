61885b284b907c91c8b7751ee1b1c5303d63f776
Pr0moK <42723354+Pr0moK@users.noreply.github.com>
2021-08-25 15:07:52 +0200

    Update PaperScissorsRockGame.py

--- PaperScissorsRockGame.py
+++ PaperScissorsRockGame.py
@@ -1,30 +1,30 @@
 import random
 
 list = ["paper", "rock", "scissors"]
-IA_choice = random.choice(list)
+AI_choice = random.choice(list)
 while True:
 
     User_Choice = input("paper, rock or scissors? ").lower()
 
-    print(f"Okay so User choose {User_Choice} and the IA choose {IA_choice} ")
+    print(f"Okay so User choose {User_Choice} and the AI choose {AI_choice} ")
 
-    if User_Choice == IA_choice:
+    if User_Choice == AI_choice:
         print("It's a draw")
     elif User_Choice == "rock":
-        if IA_choice == "scissors":
+        if AI_choice == "scissors":
             print("User won!")
         else:
-            print("IA Won")
+            print("AI Won")
     elif User_Choice == "scissors":
-        if IA_choice == "Rock":
+        if AI_choice == "Rock":
             print("User won!")
         else:
-            print("IA Won")
+            print("AI Won")
     elif User_Choice == "paper":
-        if IA_choice == "rock":
+        if AI_choice == "rock":
             print("User Won")
         else:
-            print("IA won")
+            print("AI won")
     play_again = input("Wanna play again? (Y)es/(N)o ").upper()
     if play_again != "Y":
         break
