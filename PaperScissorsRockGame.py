59996c933d40e570628daa6dc3062b6af4db7a3c
Pr0moK <pr0mok1234@gmail.com>
2023-08-26 21:40:59 +0200

    Update PaperScissorsRockGame.py
    fixed

--- PaperScissorsRockGame.py
+++ PaperScissorsRockGame.py
@@ -1,30 +1,37 @@
 import random
+import os
+
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 
 list = ["paper", "rock", "scissors"]
-AI_choice = random.choice(list)
+
 while True:
 
+    IA_choice = random.choice(list)
     User_Choice = input("paper, rock or scissors? ").lower()
 
-    print(f"Okay so User choose {User_Choice} and the AI choose {AI_choice} ")
+    print(f"Okay so User choose {User_Choice} and the IA choose {IA_choice} ")
 
-    if User_Choice == AI_choice:
+    if User_Choice == IA_choice:
         print("It's a draw")
     elif User_Choice == "rock":
-        if AI_choice == "scissors":
+        if IA_choice == "scissors":
             print("User won!")
         else:
-            print("AI Won")
+            print("IA Won")
     elif User_Choice == "scissors":
-        if AI_choice == "Rock":
+        if IA_choice == "Rock":
             print("User won!")
         else:
-            print("AI Won")
+            print("IA Won")
     elif User_Choice == "paper":
-        if AI_choice == "rock":
+        if IA_choice == "rock":
             print("User Won")
         else:
-            print("AI won")
+            print("IA won")
     play_again = input("Wanna play again? (Y)es/(N)o ").upper()
     if play_again != "Y":
         break
